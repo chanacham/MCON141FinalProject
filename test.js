1b224ed0a8242a878ba8d503aba0729a2682ee86
chanacham <93265238+chanacham@users.noreply.github.com>
2022-01-11 19:28:12 -0500

    Add files via upload
    Final version of website. Comments throughout explain the various components

---
+++ test.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  // Activate the Carousel
+  $("#myCarousel").carousel();
+
+  //  Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+
+  //  Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
